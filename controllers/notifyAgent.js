0fdf4951ae2887ec36dac1669d65d076db4f7b46
Marina Sadullaeva <marina.sadullaeva@7bits.it>
2020-04-20 18:51:29 +0600

    Removed extra arg from notifyAgent endpoint

--- controllers/notifyAgent.js
+++ controllers/notifyAgent.js
@@ -1,7 +1,7 @@
 module.exports = (req, res, next) => {
   try {
-    const { host, port, status } = req.body;
-    agents.register({ host, port, status });
+    const { host, port } = req.body;
+    agents.register({ host, port });
     res.code(200);
   } catch (e) {
     next(e);
