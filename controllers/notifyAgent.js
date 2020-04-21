2312c0d3e373de6d05f62acc885cfb376faa3a1a
Marina Sadullaeva <marina.sadullaeva@7bits.it>
2020-04-21 18:00:58 +0600

    Fixed sending status

--- controllers/notifyAgent.js
+++ controllers/notifyAgent.js
@@ -2,7 +2,7 @@ module.exports = (req, res, next) => {
   try {
     const { host, port } = req.body;
     agents.register({ host, port });
-    res.code(200);
+    res.sendStatus(200);
   } catch (e) {
     next(e);
   }
